77a500b1f631f4f9635087c877da6d73652344b6
Rayyan Zahid <rayyanzahidpk@gmail.com>
2025-02-04 00:30:02 -0800

    Change profile name and description

--- scripts/index.js
+++ scripts/index.js
@@ -27,11 +27,21 @@ const initialCards = [
 
 console.log(initialCards);
 
-let editButton = document.querySelector(".profile__edt-button");
-let editModal = document.querySelector("#edit-modal");
-let closeModalButton = document.querySelector(".modal__close-button");
+let editButton = document.querySelector(".profile__edt-button"); //select edit button
+let editModal = document.querySelector("#edit-modal"); //select edit button
+let closeModalButton = document.querySelector(".modal__close-button"); //find close buton for modal
+let editFormElement = document.querySelector(".modal__form"); //find modal form
+
+let profileName = document.querySelector(".profile__name"); // Save profile name html
+let profileDescription = document.querySelector(".profile__description"); // Save profile description html
+
+let inputPlaceholderData = document.querySelectorAll(".modal__input"); // find list of classes where inputs are done in modal
 
 function openModal() {
+  //Sets default value of fields same as the profile
+  inputPlaceholderData[0].setAttribute("value", profileName.textContent);
+  inputPlaceholderData[1].setAttribute("value", profileDescription.textContent);
+
   editModal.classList.add("modal_opened");
 }
 
@@ -39,6 +49,18 @@ function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
-editButton.addEventListener("click", openModal);
+function handleEventFormSubmit(event) {
+  event.preventDefault();
 
-closeModalButton.addEventListener("click", closeModal);
+  let inputName = editFormElement.querySelector("#name").value;
+  let inputDescription = editFormElement.querySelector("#description").value;
+
+  profileName.textContent = inputName;
+  profileDescription.textContent = inputDescription;
+
+  closeModal();
+}
+
+editButton.addEventListener("click", openModal); //Listen to open form
+closeModalButton.addEventListener("click", closeModal); //Listen to close form
+editFormElement.addEventListener("submit", handleEventFormSubmit); //Listen for form submission
